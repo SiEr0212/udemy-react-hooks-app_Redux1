a98e71c3ebc2f036f922091a753c556a688e3948
SiEr0212 <simonertel@gmx.de>
2021-05-20 23:53:02 +0200

    setting up routes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,18 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <header className="App-header"></header>
+        <header className="App-header">
+        <ul>
+          <li><NavLink>Home</NavLink></li>
+          <li><NavLink>Deposit</NavLink></li>
+          <li><NavLink>Withdraw</NavLink></li>
+        </ul>
+
+          <Route path="/" />
+          <Route path="/deposit" />
+          <Route path="/withdraw" />
+          
+        </header>
       </div>
     </BrowserRouter>
   );
