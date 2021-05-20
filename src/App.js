a6b0975c9c0a3ce45384025a7fa296f36ee2282c
SiEr0212 <simonertel@gmx.de>
2021-05-20 23:36:25 +0200

    setting up routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, NavLink } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header"></header>
+      </div>
+    </BrowserRouter>
   );
 }
 
