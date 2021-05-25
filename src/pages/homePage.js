eab1973b03cb29fbfdd5c951766748427a58c7f5
SiEr0212 <simonertel@gmx.de>
2021-05-25 18:43:03 +0200

    added a new reducer

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -2,6 +2,12 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 export default function HomePage() {
-  const balance = useSelector(state => state.balance); 
-  return <h1>Balance: {balance}</h1>;
+  const balance = useSelector((state) => state.balance);
+  return (
+    <div>
+      <h1>Balance: {balance}</h1>
+      <h1>Loan needed</h1>
+      <button className="button-style">Apply for loan</button>
+    </div>
+  );
 }
