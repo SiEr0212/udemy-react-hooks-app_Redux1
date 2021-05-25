667f107fabd46980542835f73a5d82aa350d8be6
SiEr0212 <simonertel@gmx.de>
2021-05-25 19:29:41 +0200

    changed btn style

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useSelector, useDispatch} from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 export default function HomePage() {
   const balance = useSelector((state) => state.balanceReducer.balance);
@@ -7,15 +7,20 @@ export default function HomePage() {
   const dispatch = useDispatch();
 
   const applyLoanHandle = () => {
-  dispatch({type:"APPLY"})
+    dispatch({ type: "APPLY" });
   };
 
   return (
     <div>
       <h1>Balance: {balance}</h1>
       <h1>{loan ? "Loan Applied" : "Loan Needed"}</h1>
-      <button className="button-style" disabled={loan ? true: false}  onClick={applyLoanHandle}>Apply for loan</button>
+      <button
+        className="button-style"
+        disabled={loan ? true : false}
+        onClick={applyLoanHandle}
+      >
+        Apply for loan
+      </button>
     </div>
   );
 }
- 
